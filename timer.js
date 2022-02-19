0752dd21ebcc29f6317dc126daeb2c95530082a5
yhupaul <yhupaul@hotmail.com>
2022-02-18 21:55:55 -0800

    review and change alternative way

--- timer.js
+++ timer.js
@@ -1,12 +1,50 @@
-const args = process.argv.slice(1);
-//each element in args is time set number to beep
-let timeArr = args.sort((a, b) => {
-  return a - b;
-});
+// const args = process.argv.slice(2);
+// console.log(args)
+// //each element in args is time set number to beep
+// let timeArr = args.sort((a, b) => {
+//   return a - b;
+// });
 
-let iterator = 1000;
-for (const time of timeArr) {
-  setTimeout(() => {
-    process.stdout.write('beep');
-  }, iterator * time);
+// let iterator = 1000;
+// for (const time of timeArr) {
+//   setTimeout(() => {
+//     process.stdout.write('beep');
+//   }, iterator * time);
+// }
+
+const beep = function() {
+  process.stdout.write('beep');
 }
+
+// delay is expected in seconds
+const scheduleBeep = function(delay) {
+  delay = Number(delay);
+  if (!isNaN(delay) && delay > 0) {
+    setTimeout(beep, delay * 1000);
+  }
+}
+
+const numbers = process.argv.slice(2);
+for (const num of numbers) {
+  scheduleBeep(num);
+}
+
+// const stdin = process.stdin;
+// // don't worry about these next two lines of setup work.
+// stdin.setRawMode(true);
+// stdin.setEncoding('utf8');
+
+////////////
+// Event Handling for User Input
+////////////
+
+// on any input from stdin (standard input), output a "." to stdout
+// stdin.on('data', (key) => {
+//   process.stdout.write('.');
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// });
+
+// console.log('after callback');
+
